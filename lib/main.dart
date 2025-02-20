6ca369c8b173b4e0c01ea4e00b9cc10b93114360
GAEL_SEBASTIAN_ROJAS_MATA <a.22308051281303@cbtis128.edu.mx>
2025-02-20 19:47:34 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,42 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const Mywidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Mywidgets extends StatelessWidget {
+  const Mywidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Rojas",
+              style: TextStyle(color: Color(0xffffffff)),
+            ),
+            backgroundColor: Color(0xff0c1b3d),
+            centerTitle: true,
+          ),
+          body: const Column(
+            children: <Widget>[
+              Text(
+                'Sebastian Rojas',
+                style: TextStyle(color: Color(0xff142752), fontSize: 25),
+              ),
+              Text(
+                'Mat 22308051281303 Gpo 6°J',
+                style: TextStyle(color: Color(0xff0e2c2b), fontSize: 25),
+              ),
+              Expanded(
+                child: FittedBox(
+                  child: FlutterLogo(),
+                ),
+              ),
+            ],
+          )),
+    ); // fin del material
+  } // build
+} // mi widget
